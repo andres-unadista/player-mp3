bd91df6ab0ca39d4f1e8976623a129d68565e933
andres-unadista <affernandez999@gmail.com>
2023-01-10 17:28:14 -0500

    feat: the average color  for the background is obtained

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ let $controlPlayback = document.getElementById('playback');
 let $controlNext = document.getElementById('forward');
 let $iconPause = document.getElementById('pause');
 let $iconPlay = document.getElementById('play');
+let $imageColor = document.getElementById('image-color');
 let $app = document.getElementById('app');
 let $listPlaylist = document.getElementById('playlist');
 const $audioElement = document.querySelector("#audio");
@@ -35,9 +36,40 @@ $audioElement.onloadedmetadata = function () {
   $endTime.textContent = formatTime(Math.floor($audioElement.duration));
 };
 
+$imageColor.onload = function () {
+  let average_color = getaverageColor(this);
+  handleBackground(average_color);
+};
+
 // Calling functions
 getListTitles();
 showInfoSong(music[0]);
+
+function handleBackground(color) {
+  let [red, green, blue] = color;
+  document.body.style.backgroundImage = `radial-gradient(white, rgb(${red},${green},${blue}))`;
+}
+
+function getaverageColor(imagen) {
+  let r = 0, g = 0, b = 0, count = 0, canvas, ctx, imageData, data, i, n;
+  canvas = document.createElement('canvas');
+  ctx = canvas.getContext("2d");
+  canvas.width = imagen.width;
+  canvas.height = imagen.height;
+  ctx.drawImage(imagen, 0, 0);
+  imageData = ctx.getImageData(0, 0, imagen.width, imagen.height);
+  data = imageData.data;
+  for (i = 0, n = data.length; i < n; i += 4) {
+    ++count;
+    r += data[i];
+    g += data[i + 1];
+    b += data[i + 2];
+  }
+  r = ~~(r / count);
+  g = ~~(g / count);
+  b = ~~(b / count);
+  return [r, g, b];
+}
 
 function getListTitles() {
   let index = 1;
@@ -105,6 +137,7 @@ function showInfoSong(song) {
   let background = `url('./public/images/${song.image}')`;
   $songBackground.style.backgroundImage = background;
   $audioElement.src = song.url;
+  $imageColor.src = `./public/images/${song.image}`;
 
   $songTitle.textContent = song.title;
   $songArtist.textContent = song.artist;
